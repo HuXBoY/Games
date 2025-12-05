19b0de89230eb19edcabd608f3528d15fb48f613
HuX BoY <huxboy003@gmail.com>
2025-12-05 21:09:15 +0330

    Finished V1

--- Scoundrel/main.py
+++ Scoundrel/main.py
@@ -23,6 +23,14 @@ suits_cards = {
 }
 
 
+def get_remaining_cards():
+    values = []
+    for suit in suits_cards.keys():
+        for value in suits_cards[suit]:
+            values.append(value)
+    return len(values)
+
+
 def reset_deck():
     global suits_cards
     suits_cards = {
@@ -38,15 +46,6 @@ def get_suit_color(suit: str):
         return "Black"
     else:
         return "Red"
-
-
-def get_random_card():
-    rand_suit_choice = random.choice(Suits)
-    suit_symbol = rand_suit_choice[:2]
-    suit_name = rand_suit_choice[2:]
-
-    rank = random.choice(suits_cards[suit_name])
-    return f"{rank} {suit_symbol}"
 
 
 def rank_to_name(rank: int) -> str:
@@ -88,6 +87,107 @@ def draw_card(rank: int, suit: str):
         print(f"{f.RED}ERROR \t| Card not in deck!{s.RESET_ALL}")
 
 
+def get_random_card():
+    suit = random.choice(list(suits_cards.keys()))
+    while len(suits_cards[suit]) < 1:
+        suit = random.choice(list(suits_cards.keys()))
+    rank = random.choice(suits_cards[suit])
+    return suit, rank
+
+
+def remove_card(suit, rank):
+    try:
+        suits_cards[suit].remove(rank)
+    except:
+        print(f"{f.RED}ERROR\t| {rank} of {suit} couldn't be removed.{s.RESET_ALL}")
+
+
+def start_dungeon():
+    reset_deck()
+    # DUNGEON
+    room_number = 0
+    room_cards = []
+    # PLAYER
+    health = 20
+
+    def show_health():
+        return f"{s.RESET_ALL}You have {f.GREEN if health >= 10 else f.RED}{health}{s.RESET_ALL} Health."
+    equipped_weapon = 0
+    last_card_hit_with_weapon = 15
+
+    clear_print(
+        f"{f.BLUE}Dungeon Started\t|{s.RESET_ALL} {show_health()}")
+    while 1:
+        if len(room_cards) < 2 and get_remaining_cards() > 2:
+            room_number += 1
+            while len(room_cards) < 4:
+                suit, rank = get_random_card()
+                remove_card(suit, rank)
+                rank = rank_to_name(rank)
+                room_cards.append(str(rank) + " of " + suit)
+        # WIN CONDITION
+        elif len(room_cards) < 2 and get_remaining_cards() <= 2:
+            print(
+                f"{f.GREEN}YOU CLEARED THE DUNGEON WITH {health} HEALTH REMAINING.\n|\tYou Win!\n{s.RESET_ALL}")
+            break
+
+        print(f"{f.MAGENTA}\nRoom #{room_number}: ")
+        c = 1
+        for card in room_cards:
+            print(
+                f"{f.WHITE if card.split()[-1][0].upper() in ["S", "C"] else f.RED}{c}->\t|\t{card}\t\t|\n{s.RESET_ALL}")
+            c += 1
+        c = 1
+        card_to_play = input(f"Which card to play: ")
+        try:
+            card_to_play = int(card_to_play) - 1
+            played_card = room_cards[card_to_play]
+            room_cards.remove(played_card)
+            played_card_rank = played_card.split()[0]
+            # SET CORRECT RANK FOR DAMAGE FROM FACE CARD NAMES
+            if played_card_rank == "Jack":
+                played_card_rank = 11
+            elif played_card_rank == "Queen":
+                played_card_rank = 12
+            elif played_card_rank == "King":
+                played_card_rank = 13
+            elif played_card_rank == "Ace":
+                played_card_rank = 14
+            else:
+                played_card_rank = int(played_card_rank)
+            played_card_suit = played_card.split()[-1][0].upper()
+            print(f"You Played {played_card}\t**DEBUG\t|\tRank: {played_card_rank}, Type: {type(played_card_rank)}\t|\tSuit: {played_card_suit}, Type: {type(played_card_suit)}")
+
+            if played_card_suit in ["C", "S"]:
+                if played_card_rank < last_card_hit_with_weapon and equipped_weapon > 0:
+                    health -= played_card_rank - equipped_weapon if played_card_rank - equipped_weapon > 0 else 0
+                    print(
+                        f"You have been hit for {played_card_rank} - {equipped_weapon} = {played_card_rank - equipped_weapon}. {show_health()}")
+                    last_card_hit_with_weapon = played_card_rank
+                else:
+                    health -= played_card_rank
+                    print(
+                        f"You have been hit for {played_card_rank}. {show_health()}")
+            elif played_card_suit == "D":
+                print(f"Weapon set: {played_card}")
+                equipped_weapon = int(played_card_rank)
+                last_card_hit_with_weapon = 15
+                print(f"DEBUG\t|\tEquipped weapon damage: {equipped_weapon}")
+            else:
+                health += int(played_card_rank)
+                if health > 20:
+                    health = 20
+                print(
+                    f"Healed for: {f.GREEN}{played_card_rank}{s.RESET_ALL}. {show_health()}")
+        except:
+            print(f"{f.RED}NOT A VALID INPUT{s.RESET_ALL}")
+
+        if health < 1:
+            print(f"{f.RED}YOU DIED.\n{s.RESET_ALL}")
+            input()
+            break
+
+
 def show_help():
     clear_print(f"{f.MAGENTA}Keep in mind you can always find online tutorials on how to play the game. Just search \"How to play Scoundrel Card Game\"{s.RESET_ALL}\n\n"
                 f"{f.YELLOW} Scoundrel {s.RESET_ALL}\n"
@@ -97,7 +197,8 @@ def show_help():
 # MAIN LOOP
 while 1:
     # AVAILABLE OPTIONS
-    clear_print(f"{f.GREEN}Choose One of the options: \n")
+    clear_print(
+        f"{f.BLACK}{b.CYAN} Choose One of the options: \n{s.RESET_ALL}")
     print(f"{f.GREEN}1. {f.WHITE}Start the Dungeon\n"
           f"{f.GREEN}2. {f.WHITE}Remove cards\n"
           f"{f.GREEN}3. {f.WHITE}Show current cards\n"
@@ -109,9 +210,7 @@ while 1:
 
     # OPTION CHOSEN
     if op == "1":
-        clear_print(
-            f"{f.GREEN}OK\t| Starting the dungeon . . .{s.RESET_ALL}")
-        print(f"{f.MAGENTA} Room #1: ")
+        start_dungeon()
 
         # STAY IN THE DUNGEON
         input(f"{f.BLUE}Press Enter to go to the main menu . . .{s.RESET_ALL}")
